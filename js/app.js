16a6df1a7fe1d3417b285c45e4975305f1e463fc
Arsen <arsyeghikyan@gmail.com>
2023-02-15 15:46:58 +0400

    movie v1.0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-let numberOfFilms = +prompt("Солько фильмов вы уже посмотрели?", "1");
+"use strict";
+
+const numberOfFilms = +prompt("Солько фильмов вы уже посмотрели?", "1");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+console.log(personalMovieDB);
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
